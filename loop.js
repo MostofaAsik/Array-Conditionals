0c82b1177d217140a5217489bbe16b4c51b09303
MostofaAsik <mmostofaasik@gmail.com>
2023-07-08 08:50:37 +0600

    Array conditionals

---
+++ loop.js
@@ -0,0 +1,11 @@
+for (let i = 0; i < 7; i++) {
+    // console.log(i);
+}
+
+const num = [10, 12, 13, 15]
+
+for (let i = 0; i < num.length; i++) {
+    console.log(num[i]);
+}
+
+console.log(num[2]);
